0252c2e1438759365796fb3f6168a17eb0715bf1
minhduc1706 <minhduc0879395@gmail.com>
2024-05-22 22:43:27 +0700

    slider

--- scripts/script.js
+++ scripts/script.js
@@ -51,3 +51,23 @@ currency.addEventListener("click", (e) => {
   e.preventDefault();
   currency.classList.toggle("expand");
 });
+
+// swipe the slide
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  autoplay: {
+    delay: 2000,  
+  }
+});
+
